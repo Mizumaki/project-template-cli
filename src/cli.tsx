18c960eb5a5102c4dffaddd4f75b541974a7b927
Ryota Mizumaki <statham.programming@gmail.com>
2022-06-26 13:31:00 +0900

    fix style

--- src/cli.tsx
+++ src/cli.tsx
@@ -1,4 +1,4 @@
-import { render, Text, Box } from 'ink';
+import { render, Text, Box, Newline } from 'ink';
 import { useSelectList } from './components/input/useSelectList';
 import { useTextInput } from './components/input/useTextInput';
 import { mockProjectTemplates } from './mock';
@@ -44,6 +44,32 @@ const App = () => {
             // TODO: handle isActive
             isActive: true,
           })}
+        </Box>
+      </Box>
+      <Box flexDirection='column' paddingX={1} marginY={1} borderStyle='classic' width={100}>
+        <Box justifyContent='center'>
+          <Text bold>Summary</Text>
+        </Box>
+        <Box flexDirection='column' paddingY={1}>
+          <Box flexDirection='row'>
+            <Box width={20} justifyContent='flex-end' marginRight={1}>
+              <Text bold color='green'>
+                Template:
+              </Text>
+            </Box>
+            <Text>{textInput.value}</Text>
+          </Box>
+          <Box flexDirection='row'>
+            <Box width={20} justifyContent='flex-end' marginRight={1}>
+              <Text bold color='blueBright'>
+                Distribution Path:
+              </Text>
+            </Box>
+            <Text>{textInput.value}</Text>
+          </Box>
+        </Box>
+        <Box justifyContent='center'>
+          <Text>Press enter to proceed</Text>
         </Box>
       </Box>
     </>
